8311732d29f7485e2661af89277f1361227c1899
Jmboy713 <jimin713@naver.com>
2023-11-23 09:10:34 +0900

    [Feat]    : "배우 이미지 가져오기"
    - contents models.py 에 running time 추가.
    - actors 필드에 배열 형식으로 추가.

--- contents/models.py
+++ contents/models.py
@@ -8,7 +8,7 @@ class Vod(models.Model):
     category = models.CharField(max_length=50, null=True)
     actors = models.CharField(max_length=100, null=True)
     director = models.CharField(max_length=50, null=True)
-    #runningtime = models.TimeField()
+    runningtime = models.CharField(max_length=10,null=True)
     imgpath = models.CharField(max_length=100, null=True)
     name_no_space = models.CharField(max_length=255,editable=False)
     backgroundimgpath=models.CharField(max_length=100,null=True)
