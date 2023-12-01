f855cae6640837d4b3fa5fcb3ecf9e83c6cbf39f
recordhyo <gytla1@naver.com>
2023-12-01 11:24:22 +0900

    [Refactor] : "불필요 함수 제거"

--- contents/models.py
+++ contents/models.py
@@ -15,10 +15,6 @@ class Vod(models.Model):
     count = models.IntegerField(null=True)
     choseong = models.CharField(max_length=255,null=True)
 
-    def save(self, *args, **kwargs):
-        self.name_no_space = self.name.replace(' ', '')
-        super(Vod, self).save(*args, **kwargs)
-
 
     def __str__(self) -> str:
         return self.name
