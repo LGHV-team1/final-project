e0970bd71ecf8ee56d97123e5faaf4a6e9e08834
Jmboy713 <jimin713@naver.com>
2023-12-01 18:10:41 +0900

    [Fix]     : "html 폼 안뜨는 현상 수정, 세션 에러중"

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='./static')
