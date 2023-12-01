b431af52ffd62071ddea9e3ffb89c6da5d2fe2cd
recordhyo <gytla1@naver.com>
2023-12-01 11:08:49 +0900

    [Feat] : "초성 검색 구현"

--- contents/urls.py
+++ contents/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SearchVods,SearchVodsDetail,VodReviews,VodTop5,CategorySearch
+from .views import SearchVods,SearchVodsDetail,VodReviews,VodTop5,CategorySearch, SearchVodsByChoseong
 
 urlpatterns = [
     path("<str:vodname>", SearchVods.as_view()),
+    path("search/<str:vodname>", SearchVodsByChoseong.as_view()),
     path("<int:vodid>/detail/",SearchVodsDetail.as_view()),
     path("<str:vodid>/review/",VodReviews.as_view()),
     path("category/<str:Bigcategory>/",VodTop5.as_view()),
