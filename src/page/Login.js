e9ada916ed7ed76c4b8332ed58714b701f38f30c
kkk1k <googija9818@gmail.com>
2023-12-05 12:05:51 +0900

    [Fix]: 로그인 배경 깨짐 해결
    - 로그인 배경 깨짐 해결

--- src/page/Login.js
+++ src/page/Login.js
@@ -54,9 +54,12 @@ function Login() {
   };
 
   return (
-    <body>
+   <body
+    className="bg-black "
+    style={{ backgroundImage: `url(${BGimg})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", height:"100vh" }}
+  >
       <div style={{position:"absolute"}} >
-      <img  src={BGimg} alt="background"/>
+      
       </div>
       <div className="max-w-[400px] w-[400px] mx-auto bg-transparent p-4 rounded position-relative">
         <form onSubmit={onLogin}>
