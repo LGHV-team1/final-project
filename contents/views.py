f258418cc155deb03bf3f77a4f7a8495e389eb2f
Jmboy713 <jimin713@naver.com>
2023-12-16 13:35:08 +0900

    [Feat]    : "신규 유저에 대한 2번 추천 API 작성"
    - 장르 기반 추천 API

--- contents/views.py
+++ contents/views.py
@@ -13,15 +13,7 @@ from kafka import KafkaProducer
 from pymongo import MongoClient
 from config import settings
 import json
-import pandas as pd
-from random import sample
-import numpy as np
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import scipy
-# from collections import Counter
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.neighbors import NearestNeighbors
+
 
 class MessageProducer:
     def __init__(self,broker,topic):
@@ -675,45 +667,3 @@ class CategoryPick(APIView):
             "count": vod["count"],
         }
     
-
-
-# def new_genre_based(vod_id_list):
-
-#     # vod list 가져오기
-#     vod_list = pd.read_csv('vod_list_add10_1213.csv', index_col=0)
-    
-#     # BigCategory, SmallCategory로 장르 생성
-#     vod_list['genres'] = vod_list['BigCategory'].str.replace('/', '').str.replace(' ', '') + ' ' + vod_list['SmallCategory'].str.replace('/', '').str.replace(' ', '')
-    
-#     # 장르 정보 추출
-#     cv = CountVectorizer()
-#     genres = cv.fit_transform(vod_list.genres)
-    
-#     # one-hot vector 생성
-#     genres = pd.DataFrame(genres.toarray(), columns=list(sorted(cv.vocabulary_.keys(), key=lambda x:cv.vocabulary_[x])))
-#     genres
-    
-#     # n_neighbors: 가장 가까운 n개의 이웃을 찾도록 지정합니다.
-#     nbrs = NearestNeighbors(n_neighbors=100).fit(genres)
-#     nbrs
-    
-#     # vod_id_list에 있는 vod와 가까운 10개 vod
-#     recommendation_list = pd.DataFrame()
-#     for vod_id in vod_id_list:
-#         vod = genres.iloc[vod_id, :]
-#         distances, indices = nbrs.kneighbors([vod])
-#         recommendations = vod_list.loc[indices[0], ["vod_id"]]
-#         recommendations["distance"] = distances[0]
-#         # 추천 데이터에 count 합쳐줌
-#         recommendations = pd.merge(recommendations, vod_list[['vod_id', 'counts']], on='vod_id', how="left")
-#         recommendation_list = pd.concat([recommendation_list, recommendations])
-    
-#     # 거리순, 시청량 순으로 정렬
-#     recommendation_list.sort_values(["distance", "counts"], ascending=[True, False], inplace=True)
-#     recommendation_list.drop_duplicates(inplace=True)
-#     print(recommendation_list)
-    
-#     # 상위 10개 가져옴
-#     top_10 = list(recommendation_list['vod_id'].head(10))
-    
-#     return top_10
--- recommends/urls.py
+++ recommends/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MainRecommend1,MainRecommend2,RandomRecommend,timerecommend
+from .views import MainRecommend1,MainRecommend2,RandomRecommend,timerecommend,FirstUser_Preference_2
 
 urlpatterns = [
     path("1/",MainRecommend1.as_view()),
     path("2/", MainRecommend2.as_view()),
     path("otheruser/",RandomRecommend.as_view()),
     path("3/",timerecommend.as_view()),
+    path("4/",FirstUser_Preference_2.as_view()),
 ]
